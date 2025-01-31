d6fbe9a85825ea1aa676b5acd1c087eee812882f
sengsquared <sengsquared314@gmail.com>
2025-01-31 02:33:10 -0800

    removed react fragments, assigned ChangeEvent to event arg

--- src/app/_components/SubmitBet/SubmitBet.tsx
+++ src/app/_components/SubmitBet/SubmitBet.tsx
@@ -1,49 +1,47 @@
 "use client";
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 
-interface SubmitBetProps{balance: number}
+interface SubmitBetProps {
+  balance: number;
+}
 
-export default function SubmitBet({balance}:SubmitBetProps)
- {
-  const [betAmt, setAmt] = useState(0); 
-  const updateInput = (event) => {
-    setAmt(event.target.value);
+export default function SubmitBet({ balance }: SubmitBetProps) {
+  const [betAmt, setAmt] = useState(0);
+  const updateInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setAmt(parseInt(event.target.value));
   };
-
   return (
-    <>
-      <div className="bg-emerald-900 p-[1.5rem] rounded-lg flex flex-col max-w-[375px]">
-        <div>
-          <p>Balance: ${balance}</p>
+    <div className="bg-emerald-900 p-[1.5rem] rounded-lg flex flex-col max-w-[375px]">
+      <div>
+        <p>Balance: ${balance}</p>
+      </div>
+      <div className="text-black">
+        <div className="flex gap-[.5rem]">
+          <input
+            className="rounded-md pl-[1rem]"
+            type="number"
+            placeholder="Enter Amount"
+            value={betAmt === 0 ? "" : betAmt}
+            onChange={updateInput}
+          />
+          <div className="bg-emerald-600 w-[2rem] flex justify-center items-center rounded-md">
+            <p>2x</p>
+          </div>
+          <div className="bg-green-500 max-w-[80px] h-[50px] p-1 rounded-md">
+            <p className="truncate">
+              <span className="truncate block overflow-hidden whitespace-nowrap">
+                ${betAmt && betAmt * 2}
+              </span>{" "}
+              Dollas 🤑
+            </p>
+          </div>
         </div>
-        <div className="text-black">
-          <div className="flex gap-[.5rem]">
-            <input
-              className="rounded-md pl-[1rem]"
-              type="number"
-              placeholder="Enter Amount"
-              value={betAmt === 0 ? "" : betAmt}
-              onChange={updateInput}
-            />
-            <div className="bg-emerald-600 w-[2rem] flex justify-center items-center rounded-md">
-              <p>2x</p>
-            </div>
-            <div className="bg-green-500 max-w-[80px] h-[50px] p-1 rounded-md">
-              <p className="truncate">
-                <span className="truncate block overflow-hidden whitespace-nowrap">
-                  ${betAmt && betAmt * 2}
-                </span>{" "}
-                Dollas 🤑
-              </p>
-            </div>
-          </div>
-          <div className="flex justify-end mt-[.5rem]">
-            <button className="bg-green-500 hover:bg-green-600 active:bg-green-700 p-1 px-[2.5rem] rounded-md">
-              Submit
-            </button>
-          </div>
+        <div className="flex justify-end mt-[.5rem]">
+          <button className="bg-green-500 hover:bg-green-600 active:bg-green-700 p-1 px-[2.5rem] rounded-md">
+            Submit
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
